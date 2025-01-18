c6a574af7e8eab130476d49261ea98df1a6af643
sptcnl <sptcnl@gmail.com>
2025-01-18 22:57:27 +0900

    update: JWT 내장 함수 쓰면 view 코드작성 없이 로그인 가능

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from .views import signup, login
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
     path('login/', login, name='login'),
+    # JWT 내장함수
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
