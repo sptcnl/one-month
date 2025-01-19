5471b33176a31959d3c1d9a7150482670cc03c84
sptcnl <sptcnl@gmail.com>
2025-01-19 21:47:59 +0900

    fix: 쌍따옴표로 변경

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,11 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['43.201.18.203', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    "43.201.18.203", 
+    "localhost", 
+    "127.0.0.1",
+]
 
 
 # Application definition
